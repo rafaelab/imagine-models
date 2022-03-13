1eb707bdcc32f1d238a892de6db8290f28f961a0
Hutschenreuter, Sebastian (shutsch) <hutsch@astro.ru.nl>
2022-03-13 13:57:44 +0100

    fixed cmakelist

--- test/python/test_hammurabi_wrapper.py
+++ test/python/test_hammurabi_wrapper.py
@@ -2,8 +2,6 @@ import ImagineModels as im
 import numpy as np
 
 jf12 = im.JF12MagneticField()
-s = jf12.evaluate_grid([-1.*3.0856775806e+21, 0.*3.0856775806e+21, 1.*3.0856775806e+21],
-                         [1.*3.0856775806e+21, 0.*3.0856775806e+21, -.1*3.0856775806e+21],
-                         [.1*3.0856775806e+21, 0.*3.0856775806e+21, -.1*3.0856775806e+21])
+s = jf12.evaluate_grid([-1., 0., 1.], [1., 0., -.1], [.1, 0., -.1])
 print(np.asarray(s))
 print(np.asarray(s).shape)
