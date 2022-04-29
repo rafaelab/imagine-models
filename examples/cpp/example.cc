8923921e3d5ad166e07cb9fffd1115a7c77578e8
shutsch <hutsch@astro.ru.nl>
2022-04-29 14:42:12 +0200

    cleaned up example files

--- examples/cpp/example.cc
+++ examples/cpp/example.cc
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <vector>
 
-void print_ev_pos(JF12MagneticField m_to_eval, std::vector<double> tp) {
-      std::vector<double> jf12_val = m_to_eval.evaluate_at_pos(tp);
-      std::cout << "jf12_val at ";
+void print_ev_pos(std::vector<double> mval, std::vector<double> tp) {
+
+      std::cout << "Magnetic field at position";
       for (int l = 0; l < tp.size(); l++) {
                         std::cout << tp[l] << " kpc, ";
                         }
@@ -16,14 +16,9 @@ void print_ev_pos(JF12MagneticField m_to_eval, std::vector<double> tp) {
       std::cout << "\n";
       }
 
-int main() {
-  JF12MagneticField jf12;
-  std::vector<double> test_pos{{6., 2., 0.}};
-  std::vector<double> b_val = {1.e-12, 1.e-12, 1.e-12};
-  const std::vector<double> grid_x {{2.,4.,6.,8., 0., 0., 0., 0.}};
-  const std::vector<double> grid_y {{4.,6.,8.,2., 0., 0., 0., 0.}};
-  const std::vector<double> grid_z {{-.2,-.4, .6, 0, -.1, .8, .2, 0}};
-  std::vector<std::vector<std::vector<std::vector<double>>>>  b_grid = jf12.evaluate_grid(grid_x, grid_y, grid_z);
+void print_ev_grid(std::vector<std::vector<std::vector<std::vector<double>>>>  bgrid,
+  std::vector<double> grid_x, std::vector<double> grid_y, std::vector<double> grid_z) {
+
   std::cout<< "b_grid: "  <<std::endl;
   for (int i = 0; i < b_grid.size(); i++) {
         std::cout<< "b_grid x size: " << b_grid.size() <<std::endl;
@@ -39,18 +34,40 @@ int main() {
                 }
             }
         }
-  std::cout << "\n b_arm_1: " << jf12.b_arm_1 << std::endl;
-  print_ev_pos(jf12, test_pos);
+    }
 
-  // Change some parameters
+int main() {
+  // initialize model
+  JF12MagneticField jf12;
+
+  // Define some position in Galactic cartesian coordinates (units are kpc)
+  std::vector<double> test_pos{{1., 2., 0.}};
+
+  // evaluate the position
+  std::vector<double> jf12_val = jf12.evaluate_at_pos(tp);
+  print_ev_pos(jf12_val, test_pos);
+
+  // Define grid axes in Galactic cartesian coordinates (again, units are kpc)
+  const std::vector<double> grid_x {{2., 4., 0., 1.}};
+  const std::vector<double> grid_y {{4., 6., 0.1, 0.}};
+  const std::vector<double> grid_z {{-0.2, 0.8, 0.2, 0.}};
+
+  // evaluate the grid
+  std::vector<std::vector<std::vector<std::vector<double>>>>  jf12_grid = jf12.evaluate_grid(grid_x, grid_y, grid_z);
+
+  print_ev_grid(jf12_grid, grid_x, grid_y, grid_z);
+
+  // print a parameter
+  std::cout << "\n b_arm_1: " << jf12.b_arm_1 << std::endl;
+
+
+  // Change the parameter
   jf12.b_arm_1 = 40.;
-  jf12.b_arm_2 = 40.;
-  jf12.b_arm_3 = 40.;
-  jf12.b_arm_4 = 40.;
-  jf12.b_arm_5 = 40.;
-  jf12.b_arm_6 = 40.;
-  jf12.b_arm_7 = 40.;
-  std::cout << "b_arm_1: " << jf12.b_arm_1 << std::endl;
-  print_ev_pos(jf12, test_pos);
+
+  std::cout << "\n updated b_arm_1: " << jf12.b_arm_1 << std::endl;
+
+  // Evaluate moel again
+  std::vector<double> jf12_val = jf12.evaluate_at_pos(tp);
+  print_ev_pos(jf12_val, test_pos);
   return 0;
 }
--- examples/python/example.py
+++ examples/python/example.py
@@ -1,7 +1,36 @@
 import ImagineModels as im
 import numpy as np
 
+
+# load model
+
 jf12 = im.JF12MagneticField()
-s = jf12.evaluate_grid([-1., 0., 1.], [1., 0., -.1], [.1, 0., -.1])
-print(np.asarray(s))
-print(np.asarray(s).shape)
+
+# define a position in cartesian Galactic coordinates (kpc)
+
+pos = [1., -3., 0.1]
+
+# evaluate the model at the position
+
+b_at_pos = jf12.evaluate_pos(pos)
+
+# define grid axes in cartesian Galactic coordinates (kpc)
+
+grid_x = [-3., 0., 1.]
+grid_y = [-1., 1.5, 2.]
+grid_z = [.1, 0., -1.]
+
+# evaluate grid
+b_grid = jf12.evaluate_grid(grid_x, grid_y, grid_z)
+
+# NOTE: for further processing, the evalauted grid should be converted to a numpy array, this may become unnecessary in
+# the future
+
+b_grid = np.asarray(b_grid)
+
+# change a parameter
+
+jf12.b_arm_2 = -24
+
+# evaluate model at position with updated parameter
+b_at_pos = jf12.evaluate_pos(pos)
