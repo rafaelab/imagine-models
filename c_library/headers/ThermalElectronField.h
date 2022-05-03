1a090706085483dc4332176c727170908d4bd712
shutsch <hutsch@astro.ru.nl>
2022-05-03 16:48:02 +0200

    Finished ymw16 parameters

--- c_library/headers/ThermalElectronField.h
+++ c_library/headers/ThermalElectronField.h
@@ -123,17 +123,41 @@ public:
  double t3_thetacn = 109.;
  double t3_nsg = 0.626;
  double t3_wsg = 20;
- dpuble t3_thetasg = 78.8;
- double t3_rmin[5];
- double t3_phimin[5];
- double t3_tpitch[5];
- double t3_cpitch[5];
- double t3_narm[5];
- double t3_warm[5];
- 
+ double t3_thetasg = 78.8;
+ double t3_rmin{3.35, 3.707, 3.56, 3.670, 8.21};
+ double t3_phimin{44.4, 120.0, 218.6, 330.3, 55.1};
+ double t3_tpitch{11.43, 9.84, 10.38, 10.54, 2.77};
+ double t3_cpitch{11.43, 9.84, 10.38, 10.54, 2.77};
+ double t3_narm{0.135, 0.129, 0.103, 0.116, 0.0057};
+ double t3_warm{300., 500., 300., 500., 300.};
+
 // Galactic Center
- double t4_ngc, t4_agc, t4_hgc;
- double t5_kgn, t5_ngn, t5_wgn, t5_agn;
- double t6_j_lb, t6_nlb1, t6_detlb1, t6_wlb1, t6_hlb1, t6_thetalb1,
-    t6_nlb2, t6_detlb2, t6_wlb2, t6_hlb2, t6_thetalb2;
- double t7_nli, t7_rli, t7_wli, t7_detthetali, t7_thetali;
+ double t4_ngc = 6.2;
+ double t4_agc = 160.;
+ double t4_hgc = 35.;
+
+// gum
+ double t5_kgn = 1.4;
+ double t5_ngn = 1.84;
+ double t5_wgn = 15.1;
+ double t5_agn = 125.8;
+
+// local bubble
+ double t6_j_lb = 0.480;
+ double t6_nlb1 = 1.094;
+ double t6_detlb1 = 28.4;
+ double t6_wlb1 = 14.2;
+ double t6_hlb1 = 112.9;
+ double t6_thetalb1 = 195.4;
+ double t6_nlb2 = 2.33;
+ double t6_detlb2 = 14.7;
+ double t6_wlb2 = 15.6;
+ double t6_hlb2 = 43.6;
+ double t6_thetalb2 = 278.2;
+
+// loop
+ double t7_nli = 1.907;
+ double t7_rli = 80.;
+ double t7_wli = 15.;
+ double t7_detthetali = 30.0;
+ double t7_thetali = 40.0;
