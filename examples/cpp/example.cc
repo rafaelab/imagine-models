d9d37f217994771cbb2de2f53a989c9397b2aeed
shutsch <hutsch@astro.ru.nl>
2022-05-09 16:20:44 +0200

    Updated example.cc, changed destructors for magnetic field classes

--- examples/cpp/example.cc
+++ examples/cpp/example.cc
@@ -1,17 +1,17 @@
 #include "../../c_library/headers/MagneticField.h"
-#include "../../c_library/headers/ThermalElectronField.h"
+// #include "../../c_library/headers/ThermalElectronField.h"
 #include <cassert>
 #include <iostream>
 #include <vector>
 
-void print_ev_pos(std::vector<double> mval, std::vector<double> tp) {
+void print_ev_pos(std::vector<double> mval, std::vector<double> tp, const char* title) {
 
-      std::cout << "Magnetic field at position";
-      for (int l = 0; l < tp.size(); l++) {
+      std::cout << "Magnetic field at " << title << " (";
+      for (size_t l = 0; l < tp.size(); l++) {
                         std::cout << tp[l] << " kpc, ";
                         }
-      std::cout << ":\n";
-      for (int l = 0; l < mval.size(); l++) {
+      std::cout << "):\n";
+      for (size_t l = 0; l < mval.size(); l++) {
                         std::cout << mval[l]<< ", ";
                         }
       std::cout << "\n";
@@ -21,14 +21,14 @@ void print_ev_grid(std::vector<std::vector<std::vector<std::vector<double>>>>  b
   std::vector<double> grid_x, std::vector<double> grid_y, std::vector<double> grid_z) {
 
   std::cout<< "b_grid: "  <<std::endl;
-  for (int i = 0; i < b_grid.size(); i++) {
+  for (size_t i = 0; i < b_grid.size(); i++) {
         std::cout<< "b_grid x size: " << b_grid.size() <<std::endl;
-        for (int j = 0; j < b_grid[i].size(); j++) {
+        for (size_t j = 0; j < b_grid[i].size(); j++) {
             std::cout<< "b_grid y size: " << b_grid[i].size() <<std::endl;
-            for (int k = 0; k < b_grid[i][j].size(); k++) {
+            for (size_t k = 0; k < b_grid[i][j].size(); k++) {
                 std::cout<< "b_grid z size: " << b_grid[i][j].size() <<std::endl;
                 std::cout << "x: "<< grid_x[i] << ", " << "y: "<< grid_y[j]<< ", "<< "z: "<< grid_z[k] << ", ";
-                for (int l = 0; l < b_grid[i][j][k].size(); l++) {
+                for (size_t l = 0; l < b_grid[i][j][k].size(); l++) {
                     std::cout << b_grid[i][j][k][l] << " ";
                     }
                 std::cout<<"\n";
@@ -41,12 +41,33 @@ int main() {
   // initialize model
   JF12MagneticField jf12;
 
-  // Define some position in Galactic cartesian coordinates (units are kpc)
+  // Define some positions in Galactic cartesian coordinates (units are kpc)
   std::vector<double> test_pos{{1., 2., 0.}};
+  std::vector<double> origin{{0., 0., 0.}};
+  std::vector<double> pos_on_x_axis{{.123, 0., 0.}};
+  std::vector<double> pos_on_y_axis{{0., .432, 0.}};
+  std::vector<double> pos_on_z_axis{{0., 0., 1.2}};
+  std::vector<double> pos_on_xy_axis{{.123, -.213, 0.}};
+  std::vector<double> pos_on_yz_axis{{0., -.232, -.3}};
+  std::vector<double> pos_on_xz_axis{{-.24, 0., 1.2}};
 
-  // evaluate the position
-  std::vector<double> jf12_val = jf12.evaluate_at_pos(test_pos);
-  print_ev_pos(jf12_val, test_pos);
+  // evaluate the positions
+  std::vector<double> jf12_test_pos = jf12.evaluate_at_pos(test_pos);
+  print_ev_pos(jf12_test_pos, test_pos, "test_pos");
+  std::vector<double> jf12_origin = jf12.evaluate_at_pos(origin);
+  print_ev_pos(jf12_origin, origin, "origin");
+  std::vector<double> jf12_pos_on_x_axis = jf12.evaluate_at_pos(pos_on_x_axis);
+  print_ev_pos(jf12_pos_on_x_axis, pos_on_x_axis, "pos_on_x_axis");
+  std::vector<double> jf12_pos_on_y_axis = jf12.evaluate_at_pos(pos_on_y_axis);
+  print_ev_pos(jf12_pos_on_y_axis, pos_on_y_axis, "pos_on_y_axis");
+  std::vector<double> jf12_pos_on_z_axis = jf12.evaluate_at_pos(pos_on_z_axis);
+  print_ev_pos(jf12_pos_on_z_axis, pos_on_z_axis, "pos_on_z_axis");
+  std::vector<double> jf12_pos_on_xy_axis = jf12.evaluate_at_pos(pos_on_xy_axis);
+  print_ev_pos(jf12_pos_on_xy_axis, pos_on_xy_axis, "pos_on_xy_plane");
+  std::vector<double> jf12_pos_on_yz_axis = jf12.evaluate_at_pos(pos_on_yz_axis);
+  print_ev_pos(jf12_pos_on_yz_axis, pos_on_yz_axis, "pos_on_yz_plane");
+  std::vector<double> jf12_pos_on_xz_axis = jf12.evaluate_at_pos(pos_on_xz_axis);
+  print_ev_pos(jf12_pos_on_xz_axis, pos_on_xz_axis, "pos_on_xz_plane");
 
   // Define grid axes in Galactic cartesian coordinates (again, units are kpc)
   const std::vector<double> grid_x{2., 4., 0., 1.};
@@ -69,6 +90,6 @@ int main() {
 
   // Evaluate moel again
   std::vector<double> jf12_val2 = jf12.evaluate_at_pos(test_pos);
-  print_ev_pos(jf12_val2, test_pos);
+  print_ev_pos(jf12_val2, test_pos, "updated jf12, test_pos");
   return 0;
 }
