7c2de7c9a7f9cce9f1a65781debcccfaec6dd78f
shutsch <hutsch@astro.ru.nl>
2022-05-09 13:03:49 +0200

    Updated gitignore, fixed example.cc

--- examples/cpp/example.cc
+++ examples/cpp/example.cc
@@ -1,5 +1,5 @@
 #include "../../c_library/headers/MagneticField.h"
-#include <cassert>
+
 #include <iostream>
 #include <vector>
 
@@ -10,16 +10,16 @@ void print_ev_pos(std::vector<double> mval, std::vector<double> tp) {
                         std::cout << tp[l] << " kpc, ";
                         }
       std::cout << ":\n";
-      for (int l = 0; l < jf12_val.size(); l++) {
-                        std::cout << jf12_val[l]<< ", ";
+      for (int l = 0; l < mval.size(); l++) {
+                        std::cout << mval[l]<< ", ";
                         }
       std::cout << "\n";
       }
 
-void print_ev_grid(std::vector<std::vector<std::vector<std::vector<double>>>>  bgrid,
+void print_ev_grid(std::vector<std::vector<std::vector<std::vector<double>>>> b_grid,
   std::vector<double> grid_x, std::vector<double> grid_y, std::vector<double> grid_z) {
 
-  std::cout<< "b_grid: "  <<std::endl;
+  std::cout<< "b_grid: " <<std::endl;
   for (int i = 0; i < b_grid.size(); i++) {
         std::cout<< "b_grid x size: " << b_grid.size() <<std::endl;
         for (int j = 0; j < b_grid[i].size(); j++) {
@@ -37,37 +37,37 @@ void print_ev_grid(std::vector<std::vector<std::vector<std::vector<double>>>>  b
     }
 
 int main() {
-  // initialize model
+
   JF12MagneticField jf12;
 
-  // Define some position in Galactic cartesian coordinates (units are kpc)
+
   std::vector<double> test_pos{{1., 2., 0.}};
 
-  // evaluate the position
-  std::vector<double> jf12_val = jf12.evaluate_at_pos(tp);
+
+  std::vector<double> jf12_val = jf12.evaluate_at_pos(test_pos);
   print_ev_pos(jf12_val, test_pos);
 
-  // Define grid axes in Galactic cartesian coordinates (again, units are kpc)
+
   const std::vector<double> grid_x {{2., 4., 0., 1.}};
   const std::vector<double> grid_y {{4., 6., 0.1, 0.}};
   const std::vector<double> grid_z {{-0.2, 0.8, 0.2, 0.}};
 
-  // evaluate the grid
-  std::vector<std::vector<std::vector<std::vector<double>>>>  jf12_grid = jf12.evaluate_grid(grid_x, grid_y, grid_z);
+
+  std::vector<std::vector<std::vector<std::vector<double>>>> jf12_grid = jf12.evaluate_grid(grid_x, grid_y, grid_z);
 
   print_ev_grid(jf12_grid, grid_x, grid_y, grid_z);
 
-  // print a parameter
+
   std::cout << "\n b_arm_1: " << jf12.b_arm_1 << std::endl;
 
 
-  // Change the parameter
+
   jf12.b_arm_1 = 40.;
 
   std::cout << "\n updated b_arm_1: " << jf12.b_arm_1 << std::endl;
 
-  // Evaluate moel again
-  std::vector<double> jf12_val = jf12.evaluate_at_pos(tp);
-  print_ev_pos(jf12_val, test_pos);
+
+  std::vector<double> jf12_val_updated = jf12.evaluate_at_pos(test_pos);
+  print_ev_pos(jf12_val_updated, test_pos);
   return 0;
 }
