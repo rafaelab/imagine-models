83be97ac1167746aa8e530238ef89b7f274e262f
shutsch <hutsch@astro.ru.nl>
2022-11-01 16:14:39 +0100

    Comitting old local changes

--- python_wrapper/abstract_trampoline.h
+++ python_wrapper/abstract_trampoline.h
@@ -12,3 +12,9 @@ public:
     using VectorField::VectorField; // Inherit constructors
     std::vector<double> evaluate_model(const double& x, const double& y, const double& z) const override {PYBIND11_OVERRIDE_PURE(std::vector<double>, VectorField, evaluate_model, x, y, z); }
 };
+
+class PyScalarField : public ScalarField {
+public:
+    using ScalarField::ScalarField; // Inherit constructors
+    double evaluate_model(const double& x, const double& y, const double& z) const override {PYBIND11_OVERRIDE_PURE(std::vector<double>, ScalarField, evaluate_model, x, y, z); }
+};
