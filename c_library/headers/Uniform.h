11d56df49ec0edae25803535158b9dbdc90fe1f6
shutsch <hutsch@astro.ru.nl>
2023-04-06 21:26:55 +0200

    param struct for uniform

--- c_library/headers/Uniform.h
+++ c_library/headers/Uniform.h
@@ -4,17 +4,26 @@
 #include "Field.h"
 #include "RegularField.h"
 
+struct UniformParams : Params {
+    //parameter_names = {"ampx", "ampy"};
+
+    double bx = 0.;
+    double by = 0.;
+    double bz = 0.;
+
+    };
+
+
+
 class UniformMagneticField : public RegularVectorField  {
     protected:
         bool DEBUG = false;
     public:
         using RegularVectorField :: RegularVectorField;
 
-        double bx = 0.;
-        double by = 0.;
-        double bz = 0.;
+        UniformParams param;
 
         std::array<double, 3>  at_position (const double &x, const double &y, const double &z) const {
-            return std::array<double, 3> {bx, by, bz};
+            return std::array<double, 3> {param.bx, param.by, param.bz};
         }
  };
--- python_wrapper/include/regular/UniformWrapper.h
+++ python_wrapper/include/regular/UniformWrapper.h
@@ -6,6 +6,14 @@ namespace py = pybind11;
 using namespace pybind11::literals;
 
 void Uniform(py::module_ &m) {
+
+py::class_<UniformParams>(m, "UniformParams")
+        .def(py::init<>())
+        .def_readwrite("bx", &UniformParams::bx)
+        .def_readwrite("by", &UniformParams::by)
+        .def_readwrite("bz", &UniformParams::bz);
+
+
 py::class_<UniformMagneticField, RegularVectorField>(m, "UniformMagneticField")
     .def(py::init<>())
     .def(py::init<std::vector<double> &, std::vector<double> &, std::vector<double> &>())
@@ -13,7 +21,7 @@ py::class_<UniformMagneticField, RegularVectorField>(m, "UniformMagneticField")
 
     .def("at_position", &UniformMagneticField::at_position,  "x"_a, "y"_a, "z"_a, py::return_value_policy::move)
 
-    .def_readwrite("bx", &UniformMagneticField::bx)
-    .def_readwrite("by", &UniformMagneticField::by)
-    .def_readwrite("bz", &UniformMagneticField::bz);
+    .def_readwrite("param", &UniformMagneticField::param);
+
+
 }
