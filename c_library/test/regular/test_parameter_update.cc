6f0b3cb322a8a5c821675744029406d1ad6995e8
shutsch <hutsch@astro.ru.nl>
2023-09-06 18:49:31 +0200

    type update in tests

--- c_library/test/regular/test_parameter_update.cc
+++ c_library/test/regular/test_parameter_update.cc
@@ -8,18 +8,18 @@
 
 void test_parameter_update() {
     UniformMagneticField umf = UniformMagneticField();
-    assert (umf.bx == 0.);
-    assert (umf.by == 0.);
-    assert (umf.bz == 0.);
+    assert (umf.param.bx == 0.);
+    assert (umf.param.by == 0.);
+    assert (umf.param.bz == 0.);
 
-    std::array<double, 3> zeros{0., 0., 0.};
+    vector zeros{{0., 0., 0.}};
     
     assert (umf.at_position(2.4, 2.1, -.2) == zeros);
     
-    umf.bx = -3.2; 
+    umf.param.bx = -3.2; 
     
-    assert (umf.bx == -3.2); 
-    std::array<double, 3> updated{-3.2, 0., 0.};
+    assert (umf.param.bx == -3.2); 
+    vector updated{{-3.2, 0., 0.}};
     
     assert (umf.at_position(2.4, 2.1, -.2) == updated);
 
