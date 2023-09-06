d12de1f655822ec433ed606c5d2b0cdb8676656a
shutsch <hutsch@astro.ru.nl>
2023-09-06 14:07:09 +0200

    TT derivative

--- c_library/headers/ImagineModels/TinyakovTkachev.h
+++ c_library/headers/ImagineModels/TinyakovTkachev.h
@@ -5,20 +5,43 @@
 #include "RegularField.h"
 
 
+struct TTParams : Params {
+
+    number b_b0 = 1.4; // muG
+    number b_Rsun = 8.5; // kpc
+    double b_r_max = 20.; // kpc
+    double b_r_min = 4.; // kpc
+    number b_d = -0.5; // kpc
+    number b_z0 = 1.5; // kpc
+    number b_p = -8; // degree
+
+    };
+
+
 //Tinyakov and Tkachev (TT) https://arxiv.org/abs/astro-ph/0102101, implementation of https://arxiv.org/pdf/astro-ph/0510444.pdf (Kachelriess et al.)
 class TTMagneticField : public RegularVectorField  {
     protected:
         bool DEBUG = false;
+        vector  _at_position(const double &x, const double &y, const double &z, const TTParams &p) const;
     public:
         using RegularVectorField :: RegularVectorField;
-        double b_b0 = 1.4; // muG
-        double b_Rsun = 8.5; // kpc
-        double b_r_max = 20.; // kpc
-        double b_r_min = 4.; // kpc
-        double b_d = -0.5; // kpc
-        double b_z0 = 1.5; // kpc
-        double b_p = -8; // degree
 
+        TTParams param;
 
-        std::array<double, 3>  at_position (const double &x, const double &y, const double &z) const;
+        vector at_position(const double &x, const double &y, const double &z) const {
+            return _at_position(x, y, z, this->param);
+        }
+        
+
+        #if autodiff_FOUND
+            Eigen::MatrixXd _derivative(const double &x, const double &y, const double &z,  TTParams &p) {
+                vector out;
+                Eigen::MatrixXd deriv = ad::jacobian([&](auto _x, auto _y, auto _z, auto _p) {return this->_at_position(_x, _y, _z, _p);}, ad::wrt(p.b_b0, p.b_d, p.b_p, p.b_Rsun, p.b_z0), ad::at(x, y, z, p), out);  
+                return deriv;
+            }
+
+            Eigen::MatrixXd derivative(const double &x, const double &y, const double &z) {
+                return _derivative(x, y, z, this->param);
+            }
+        #endif       
  };
--- c_library/source/tinyakovtkachev.cc
+++ c_library/source/tinyakovtkachev.cc
@@ -1,50 +1,56 @@
 #include <cmath>
-#include "../headers/TinyakovTkachev.h"
 
+#include "TinyakovTkachev.h"
 #include "helpers.h"
 
 vector TTMagneticField::_at_position(const double &x, const double &y, const double &z, const TTParams &p) const { 
+    
     double r = sqrt(x*x + y*y);
+
+    vector B_vec3{{0, 0, 0}};
+    if (r <= p.b_r_max) {
+        return B_vec3;
+    }
+
     double phi = atan2(y, x);
 
-    double beta = 1./tan(p.b_p);
+    number beta = 1./tan(p.b_p);
     if(abs(p.b_p) < 1.e-16) { 
-        beta=1.;
+        beta = 1.;
         }
 
-    double phase = (beta * log(1. + p.b_d / p.b_Rsun)) - M_PI/2.;
+    number phase = (beta * log(1. + p.b_d / p.b_Rsun)) - M_PI/2.;
     //  double epsilon0=(b5_Rsun+b5_d)*exp(-(M_PI/2.)*tan(b5_p)); <-- hammurabi comment
 
     double sign;
     if (z < 0) { 
-        sign=1.; 
+        sign = 1.; 
     }
     else { 
-        sign=-1.;
+        sign = -1.;
     }
-    double f_z = sign * exp(-(abs(z) / p.b_z0));
+    number f_z = sign * exp(-(abs(z) / p.b_z0));
 
     // there is a factor 1/cos(phase) difference between
     // the original TT and Kachelriess. <-- hammurabi comment
     //double b_r=b_b0*(b_Rsun/(r));
-    double b_r = p.b_b0 * (p.b_Rsun/(r * cos(phase)));
+    number b_r = p.b_b0 * (p.b_Rsun/(r * cos(phase)));
 
     //if(r<b5_r_min) {b_r=b5_b0*(b5_Rsun/(b5_r_min));} <-- hammurabi comment
     if (r < p.b_r_min) {
         b_r = p.b_b0 * (p.b_Rsun / (p.b_r_min * cos(phase)));
         }
   
-    double B_r_phi = b_r * cos(phi - beta * log(r / p.b_Rsun) + phase);
+    number B_r_phi = b_r * cos(phi - beta * log(r / p.b_Rsun) + phase);
     //  if(r<1.e-26){B_r_phi = b_r*std::cos(phi+phase);} <-- hammurabi comment
 
     // B-field in cylindrical coordinates: <-- hammurabi comment
-    vector B_cyl{{B_r_phi * sin(p.b_p) * f_z, B_r_phi * cos(p.b_p) * f_z , 0.}};
+    vector B_cyl{{B_r_phi * sin(p.b_p) * f_z, 
+                  B_r_phi * cos(p.b_p) * f_z , 
+                  0.}
+                };
 
-    vector B_vec3{{0, 0, 0}};
-    
-    if (r <= p.b_r_max) {
-        Cyl2Cart(phi, B_cyl, B_vec3);
-    }
+    B_vec3 = Cyl2Cart(phi, B_cyl);
 
     return B_vec3;
-    }
+    };
