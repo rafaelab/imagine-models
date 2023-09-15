322984263c412d81f99ae23521aed5e22de5554b
shutsch <hutsch@astro.ru.nl>
2023-09-15 12:08:20 +0200

    JF12 derivative

--- c_library/headers/ImagineModels/RegularJF12.h
+++ c_library/headers/ImagineModels/RegularJF12.h
@@ -6,67 +6,66 @@
 #include <cassert>
 #include <iostream>
 
-#include "Field.h"
 #include "RegularField.h"
-#include "param.h"
 
+class JF12MagneticField : public RegularVectorField
+{
+protected:
+  vector _at_position(const double &x, const double &y, const double &z, const JF12MagneticField &p) const;
 
-struct JF12RegularParams : Params {
-    number b_arm_1 = 0.1;
-    number b_arm_2 = 3.0;
-    number b_arm_3 = -0.9;
-    number b_arm_4 = -0.8;
-    number b_arm_5 = -2.0;
-    number b_arm_6 = -4.2;
-    number b_arm_7 = 0.0;
-    number b_ring = 0.1;
-    number h_disk = 0.40;
-    number w_disk = 0.27;
-    // toroidal halo parameters
-    number Bn = 1.4;
-    number Bs = -1.1;
-    number rn = 9.22;
-    number rs = 16.7;
-    number wh = 0.20;
-    number z0 = 5.3;
-    // X-field parameters
-    number B0_X = 4.6;
-    number Xtheta_const = 49;
-    number rpc_X = 4.8;
-    number r0_X= 2.9;
-    };
+#if autodiff_FOUND
+  Eigen::MatrixXd _jac(const double &x, const double &y, const double &z, JF12MagneticField &p) const;
+#endif
 
+public:
+  using RegularVectorField ::RegularVectorField;
 
-class JF12MagneticField : public RegularVectorField {
-  protected:
-    bool DEBUG = false;
+  // define fixed parameters
+  const double Rmax = 20;   // outer boundary of GMF
+  const double rho_GC = 1.; // interior boundary of GMF
 
-    vector _at_position(const double &x, const double &y, const double &z, const JF12RegularParams &p) const;
-  
-  public:
-    using RegularVectorField :: RegularVectorField;
+  // fixed disk parameters
+  const double inc = 11.5; // inclination, in degrees
+  const double rmin = 5.;  // outer boundary of the molecular ring region
+  const double rcent = 3.; // inner boundary of the molecular ring region (field is
+                           // zero within this region)
 
-    JF12RegularParams param;
+  number b_arm_1 = 0.1;
+  number b_arm_2 = 3.0;
+  number b_arm_3 = -0.9;
+  number b_arm_4 = -0.8;
+  number b_arm_5 = -2.0;
+  number b_arm_6 = -4.2;
+  number b_arm_7 = 0.0;
+  number b_ring = 0.1;
+  number h_disk = 0.40;
+  number w_disk = 0.27;
+  // toroidal halo parameters
+  number Bn = 1.4;
+  number Bs = -1.1;
+  number rn = 9.22;
+  number rs = 16.7;
+  number wh = 0.20;
+  number z0 = 5.3;
+  // X-field parameters
+  number B0_X = 4.6;
+  number Xtheta_const = 49;
+  number rpc_X = 4.8;
+  number r0_X = 2.9;
+#if autodiff_FOUND
+  const std::set<std::string> all_diff{"b_arm_1", "b_arm_2", "b_arm_3", "b_arm_4", "b_arm_5", "b_arm_6", "b_arm_7", "b_ring", "h_disk", "w_disk", "Bn", "Bs", "rn", "rs", "wh", "z0", "B0_X", "Xtheta_const", "rpc_X", "r0_X"};
+  std::set<std::string> active_diff{"b_arm_1", "b_arm_2", "b_arm_3", "b_arm_4", "b_arm_5", "b_arm_6", "b_arm_7", "b_ring", "h_disk", "w_disk", "Bn", "Bs", "rn", "rs", "wh", "z0", "B0_X", "Xtheta_const", "rpc_X", "r0_X"};
 
-    vector at_position(const double &x, const double &y, const double &z) const {
-        return _at_position(x, y, z, this->param);
+  Eigen::MatrixXd derivative(const double &x, const double &y, const double &z)
+  {
+    return _jac(x, y, z, *this);
   }
+#endif
 
-  #if autodiff_FOUND
-    Eigen::MatrixXd _derivative(const double &x, const double &y, const double &z,  JF12RegularParams &p) {
-        vector out;
-        Eigen::MatrixXd deriv = ad::jacobian([&](auto _x, auto _y, auto _z, auto _p) {return this->_at_position(_x, _y, _z, _p);}, ad::wrt(p.b_arm_1, p.b_arm_2, p.b_arm_3, p.b_arm_4, p.b_arm_5, p.b_arm_6, p.b_arm_7,
-        p.b_ring, p.h_disk, p.w_disk, 
-        p.Bn, p.Bs, p.rn, p.rs, p.wh, p.z0,
-        p.B0_X, p.Xtheta_const, p.rpc_X, p.r0_X), ad::at(x, y, z, p), out);  
-        return deriv;
-    }
-
-    Eigen::MatrixXd derivative(const double &x, const double &y, const double &z) {
-        return _derivative(x, y, z, this->param);
-    }
-  #endif
-
+  vector at_position(const double &x, const double &y, const double &z) const
+  {
+    return _at_position(x, y, z, *this);
+  }
 };
 
 #endif
--- python_wrapper/include/regular/RegularJF12Wrapper.h
+++ python_wrapper/include/regular/RegularJF12Wrapper.h
@@ -5,47 +5,48 @@
 
 #include "RegularJF12.h"
 
-
-void RegularJF12(py::module_ &m) {
-    py::class_<JF12RegularParams>(m, "JF12RegularParams")
-        .def(py::init<>())
-        .def_readwrite("b_arm_1", &JF12RegularParams::b_arm_1)
-        .def_readwrite("b_arm_2", &JF12RegularParams::b_arm_2)
-        .def_readwrite("b_arm_3", &JF12RegularParams::b_arm_3)
-        .def_readwrite("b_arm_4", &JF12RegularParams::b_arm_4)
-        .def_readwrite("b_arm_5", &JF12RegularParams::b_arm_5)
-        .def_readwrite("b_arm_6", &JF12RegularParams::b_arm_6)
-        .def_readwrite("b_arm_7", &JF12RegularParams::b_arm_7)
-        .def_readwrite("b_ring", &JF12RegularParams::b_ring)
-        .def_readwrite("h_disk", &JF12RegularParams::h_disk)
-        .def_readwrite("w_disk", &JF12RegularParams::w_disk)
-        .def_readwrite("Bn", &JF12RegularParams::Bn)
-        .def_readwrite("Bs", &JF12RegularParams::Bs)
-        .def_readwrite("rn", &JF12RegularParams::rn)
-        .def_readwrite("rs", &JF12RegularParams::rs)
-        .def_readwrite("wh", &JF12RegularParams::wh)
-        .def_readwrite("z0", &JF12RegularParams::z0)
-        .def_readwrite("B0_X", &JF12RegularParams::B0_X)
-        .def_readwrite("Xtheta_const", &JF12RegularParams::Xtheta_const)
-        .def_readwrite("rpc_X", &JF12RegularParams::rpc_X)
-        .def_readwrite("r0_X", &JF12RegularParams::r0_X);
-
-
+void RegularJF12(py::module_ &m)
+{
     py::class_<JF12MagneticField, RegularVectorField>(m, "JF12RegularField")
         .def(py::init<>())
         .def(py::init<std::vector<double> &, std::vector<double> &, std::vector<double> &>())
         .def(py::init<std::array<int, 3> &, std::array<double, 3> &, std::array<double, 3> &>())
 
-        .def("at_position", [](JF12MagneticField &self, double x, double y, double z)  {
+        .def_readwrite("b_arm_1", &JF12MagneticField::b_arm_1)
+        .def_readwrite("b_arm_2", &JF12MagneticField::b_arm_2)
+        .def_readwrite("b_arm_3", &JF12MagneticField::b_arm_3)
+        .def_readwrite("b_arm_4", &JF12MagneticField::b_arm_4)
+        .def_readwrite("b_arm_5", &JF12MagneticField::b_arm_5)
+        .def_readwrite("b_arm_6", &JF12MagneticField::b_arm_6)
+        .def_readwrite("b_arm_7", &JF12MagneticField::b_arm_7)
+        .def_readwrite("b_ring", &JF12MagneticField::b_ring)
+        .def_readwrite("h_disk", &JF12MagneticField::h_disk)
+        .def_readwrite("w_disk", &JF12MagneticField::w_disk)
+        .def_readwrite("Bn", &JF12MagneticField::Bn)
+        .def_readwrite("Bs", &JF12MagneticField::Bs)
+        .def_readwrite("rn", &JF12MagneticField::rn)
+        .def_readwrite("rs", &JF12MagneticField::rs)
+        .def_readwrite("wh", &JF12MagneticField::wh)
+        .def_readwrite("z0", &JF12MagneticField::z0)
+        .def_readwrite("B0_X", &JF12MagneticField::B0_X)
+        .def_readwrite("Xtheta_const", &JF12MagneticField::Xtheta_const)
+        .def_readwrite("rpc_X", &JF12MagneticField::rpc_X)
+        .def_readwrite("r0_X", &JF12MagneticField::r0_X)
+#if autodiff_FOUND
+        .def_readwrite("active_diff", &JF12MagneticField::active_diff)
+        .def_readonly("all_diff", &JF12MagneticField::all_diff)
+
+        .def("derivative", [](JF12MagneticField &self, double x, double y, double z)
+            { return self.derivative(x, y, z); },
+            "x"_a, "y"_a, "z"_a, py::return_value_policy::move)
+#endif
+
+        .def("at_position", [](JF12MagneticField &self, double x, double y, double z)
+            {
             vector f = self.at_position(x, y, z);
-            auto tp = std::make_tuple(f[0], f[1], f[2]);
-            return tp;},
-            "x"_a, "y"_a, "z"_a, py::return_value_policy::take_ownership)
-
-        .def("derivative", &JF12MagneticField::derivative, "x"_a, "y"_a, "z"_a, py::return_value_policy::move)
-
-        .def_readwrite("param", &JF12MagneticField::param);
-
+            return std::make_tuple(f[0], f[1], f[2]); 
+            },
+            "x"_a, "y"_a, "z"_a, py::return_value_policy::take_ownership);
 }
 
 #endif
