c11c5fe5ae0528ddd60d1fb18a8c360a196d1cc4
shutsch <hutsch@astro.ru.nl>
2023-09-27 00:58:41 +0200

    updated Readme uprdated demos

--- demos/python/model_examples/plotting_helpers.py
+++ demos/python/model_examples/plotting_helpers.py
@@ -10,7 +10,7 @@ except ImportError:
 
 plt.ion()
 
-def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=True):
+def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=True, show_labels=True, save_fig=False):
    
     if has_cmasher:
         cmap = getattr(cm, "prinsenvlag") 
@@ -20,9 +20,6 @@ def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=T
     dims = [0, 1, 2]
     dims.remove(slice_dim)
     
-    dims_label = ['x', 'y', 'z']
-    comp_label = dims_label[vec_dim]
-    dims_label.remove(comp_label)
 
     slices = [slice(0, shp[0], None), slice(0, shp[1], None), slice(0, shp[2], None)]
     cut_index = int(shp[slice_dim]/2)
@@ -30,21 +27,32 @@ def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=T
     slices = tuple(slices)
     
     ax.imshow(np.squeeze(array[vec_dim][slices]).T, cmap=cmap, origin='lower', vmin=vmin, vmax=vmax)
-    
+
+    dims_label = ['x', 'y', 'z']
+    comp_label = dims_label[vec_dim]
+    dims_label.remove(comp_label)
+
     xticks_label = [int(rfp[dims[0]] + i*shp[dims[0]]/5*inc[dims[0]]) for i in range(6)]
     xticks_loc = [i*shp[dims[0]]/5 for i in range(6)]
     yticks_label = [int(rfp[dims[1]] + i*shp[dims[1]]/5*inc[dims[1]]) for i in range(6)]
     yticks_loc = [i*shp[dims[1]]/5 for i in range(6)]
-    
-    ax.set_yticks(yticks_loc, labels=yticks_label)
-    ax.set_ylabel("kpc")
-    ax.set_xticks(xticks_loc, labels=xticks_label)
-    ax.set_xlabel("kpc")
-    
-    ax.set_title(comp_label + " component in the  " + dims_label[0] + "-" + dims_label[1] + " plane")
+        
+
+    if show_labels:
+
+        ax.set_yticks(yticks_loc, labels=yticks_label)
+        ax.set_ylabel("kpc")
+        ax.set_xticks(xticks_loc, labels=xticks_label)
+        ax.set_xlabel("kpc")    
+        ax.set_title(comp_label + " component in the  " + dims_label[0] + "-" + dims_label[1] + " plane")
+    else:
+        plt.axis('off')
+
+
     if show_cbar:
         fig.colorbar(ax.images[0], orientation="horizontal")
     plt.tight_layout()
-    
+    if save_fig:
+        plt.savefig(comp_label + "_component_" + dims_label[0] + "_" + dims_label[1] + "_plane")    
     plt.show()
     plt.close()
