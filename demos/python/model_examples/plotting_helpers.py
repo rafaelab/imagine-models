f48fa9c53c07694ff015be0b53d1c64fc3119367
teresakarolin <teresa.bister@gmail.com>
2023-10-05 14:19:39 +0200

    add quiver and more info to plotting

--- demos/python/model_examples/plotting_helpers.py
+++ demos/python/model_examples/plotting_helpers.py
@@ -10,21 +10,24 @@ except ImportError:
 
 plt.ion()
 
-def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=True, show_labels=True, save_fig=False):
+def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=True, show_labels=True,
+               save_fig=False, field_name=None, quiver=False):
     """
-    produces a plot showing a slice through GMF model
+    produces a plot showing a slice through GMF model. The slice goes throgh the middle of array in the respective dimension slice_dim
 
     :param array: GMF field in grid form e.g. from on_grid()
-    :param vec_dim: which dimenion of vec(B) to display, can be 0, 1, 2. If set to any other value, the amplitude of B is taken.
+    :param vec_dim: which dimension of vec(B) to display, can be 0, 1, 2. If set to any other value, the amplitude of B is taken.
     :param slice_dim: dimension on which to slice, e.g. 0 will give a slice through the y-z plane
     :param shp: shape of the image to plot, given as a list containing number of cells in [x, y, z]
     :param rfp: reference point = location of the smallest coordinate value in each direction, as list [refx, refy, refz]
     :param inc: increment in each dimension = size of cells used for plot, as list [incx, incy, incz]
-    :param vmin: minimum value on color bar (magnetic field in G)
-    :param vmax: maximum value on color bar (magnetic field in G)
+    :param vmin: minimum value on color bar (magnetic field in muG)
+    :param vmax: maximum value on color bar (magnetic field in muG)
     :param show_cbar: show color bar
     :param show_labels: show labels on plotted axes
     :param save_fig: saves figure automatically
+    :param field_name: if not None: displays name of field in plot
+    :param quiver: adds field vectors displayed as arrows to plot
     """
    
     if has_cmasher:
@@ -50,8 +53,16 @@ def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=T
     except IndexError:
         comp_label = 'amplitude'
         ax.imshow(np.squeeze(np.linalg.norm(array, axis=0)[slices]).T, cmap='Reds', origin='lower', vmin=0, vmax=vmax)
-        
-    dims_label.remove(dims_label[slice_dim])
+    
+    if quiver:  # add arrows indicating B-field direction
+        coord1 = [i for i in range(shp[dims[0]])][::5]  # only every fifth cell gets an arrow for better overview
+        coord2 = [i for i in range(shp[dims[1]])][::5]
+        B1 = np.squeeze(array[dims[0]][slices])[::5, ::5]
+        B2 = np.squeeze(array[dims[1]][slices])[::5, ::5]
+        plt.quiver(coord1, coord1, B1.T, B2.T, pivot='mid')
+    
+    slice_dim_label = dims_label[slice_dim]
+    dims_label.remove(slice_dim_label)
     
 
     xticks_label = [int(rfp[dims[0]] + i*shp[dims[0]]/4*inc[dims[0]]) for i in range(5)]
@@ -64,7 +75,11 @@ def plot_slice(array, vec_dim, slice_dim, shp, rfp, inc, vmin, vmax, show_cbar=T
         ax.set_yticks(yticks_loc, labels=yticks_label)
         ax.set_ylabel(r"$%s$ / kpc" % dims_label[1])
         ax.set_xticks(xticks_loc, labels=xticks_label)
-        ax.set_xlabel(r"$%s$ / kpc" % dims_label[0])    
+        ax.set_xlabel(r"$%s$ / kpc" % dims_label[0])
+        keyword = '%s = %.1f kpc' % (slice_dim_label, rfp[slice_dim]+cut_index*inc[slice_dim]) 
+        keyword = field_name + ', ' + keyword if field_name is not None else keyword
+        ax.text(0, 1, keyword,
+                va='bottom', transform = ax.transAxes)
     else:
         plt.axis('off')
 
