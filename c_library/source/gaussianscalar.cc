df0ae781061999f3833b51f6cf726bb900e9d2a8
shutsch <hutsch@astro.ru.nl>
2023-10-17 16:51:34 +0200

    padding removal in gauss scalar

--- c_library/source/gaussianscalar.cc
+++ c_library/source/gaussianscalar.cc
@@ -7,14 +7,15 @@
 void GaussianScalarField::_on_grid(double* val, const std::array<int, 3> &shp, const std::array<double, 3> &rpt, const std::array<double, 3> &inc, const int seed) {
 
       fftw_complex* val_comp = construct_plans(val, shp);;
-        
       draw_random_numbers_complex(val_comp, shp, inc, seed);
       
       fftw_execute(c2r);
-
+      std::array<int, 3> padded_shp = {shp[0],  shp[1],  2*(shp[2]/2 + 1)}; 
+      int pad =  padded_shp[2] - shp[2];
+      remove_padding(val, shp, pad);
       // normalize and add mean
-      std::array<int, 3> padded_shape = {shp[0],  shp[1],  2*(shp[2]/2 + 1)}; 
-      int gs_padded = grid_size(padded_shape);
+
+      int gs_padded = grid_size(padded_shp);
       int gs = grid_size(shp);
       for (int s = 0; s < gs_padded; ++s) {
         val[s] = val[s]/std::sqrt(gs) + mean;  
