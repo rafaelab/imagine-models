8678d1dc043fd9ccb666752ed933c5da76001183
shutsch <hutsch@astro.ru.nl>
2023-10-19 14:51:12 +0200

    Added uniform density field

--- c_library/headers/ImagineModels/Uniform.h
+++ c_library/headers/ImagineModels/Uniform.h
@@ -42,4 +42,39 @@ public:
     }
 };
 
+
+class UniformDensityField : public RegularScalarField
+{
+protected:
+    number _at_position(const double &x, const double &y, const double &z, const UniformDensityField &p) const
+    {
+        return p.n0;
+    }
+
+#if autodiff_FOUND
+    Eigen::MatrixXd _jac(const double &x, const double &y, const double &z, UniformDensityField &p) const
+    {
+        return Eigen::MatrixXd::Identity(1, 1);
+    }
+#endif
+public:
+    using RegularScalarField ::RegularScalarField;
+
+    number n0 = 0.;
+
+#if autodiff_FOUND
+    const std::set<std::string> all_diff{"n0"};
+    std::set<std::string> active_diff{"n0"};
+
+    Eigen::MatrixXd derivative(const double &x, const double &y, const double &z)
+    {
+        return _jac(x, y, z, *this);
+    }
+#endif
+    number at_position(const double &x, const double &y, const double &z) const
+    {
+        return _at_position(x, y, z, *this);
+    }
+};
+
 #endif
--- python_wrapper/include/regular/UniformWrapper.h
+++ python_wrapper/include/regular/UniformWrapper.h
@@ -33,6 +33,24 @@ void Uniform(py::module_ &m)
             return std::make_tuple(f[0], f[1], f[2]); 
             },
             "x"_a, "y"_a, "z"_a, py::return_value_policy::take_ownership);
+
+    py::class_<UniformDensityField, RegularScalarField>(m, "UniformDensityField")
+        .def(py::init<>())
+        .def(py::init<std::vector<double> &, std::vector<double> &, std::vector<double> &>())
+        .def(py::init<std::array<int, 3> &, std::array<double, 3> &, std::array<double, 3> &>())
+
+        .def_readwrite("n0", &UniformDensityField::n0)
+
+#if autodiff_FOUND
+        .def_readwrite("active_diff", &UniformDensityField::active_diff)
+        .def_readonly("all_diff", &UniformDensityField::all_diff)
+
+        .def("derivative", [](UniformDensityField &self, double x, double y, double z)
+            { return self.derivative(x, y, z); },
+            "x"_a, "y"_a, "z"_a, py::return_value_policy::move)
+#endif
+
+        .def("at_position", &UniformDensityField::at_position, "x"_a, "y"_a, "z"_a, py::return_value_policy::move);
 }
 
 #endif
